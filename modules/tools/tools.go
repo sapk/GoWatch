e63747f2eb4d63a99f4a0115dedb8e0997fc4d3f
Antoine GIRARD <sapk@sapk.fr>
2015-06-07 18:53:57 +0200

    Add ressources monitoring in admin dashboard

--- modules/tools/tools.go
+++ modules/tools/tools.go
@@ -1,7 +1,141 @@
 package tools
 
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
 //ValidIPAddressRegex regex for validate IP
 const ValidIPAddressRegex = `(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])`
 
 //ValidHostAddressRegex regex for validate HostAddress
 const ValidHostAddressRegex = `^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$`
+
+//(sources from gogs project)
+func logn(n, b float64) float64 {
+	return math.Log(n) / math.Log(b)
+}
+
+//(sources from gogs project)
+func humanateBytes(s uint64, base float64, sizes []string) string {
+	if s < 10 {
+		return fmt.Sprintf("%dB", s)
+	}
+	e := math.Floor(logn(float64(s), base))
+	suffix := sizes[int(e)]
+	val := float64(s) / math.Pow(base, math.Floor(e))
+	f := "%.0f"
+	if val < 10 {
+		f = "%.1f"
+	}
+
+	return fmt.Sprintf(f+"%s", val, suffix)
+}
+
+// FileSize calculates the file size and generate user-friendly string. (sources from gogs project)
+func FileSize(s int64) string {
+	sizes := []string{"B", "KB", "MB", "GB", "TB", "PB", "EB"}
+	return humanateBytes(uint64(s), 1024, sizes)
+}
+
+//TimeSinceHuman return a more human readable time duration
+func TimeSinceHuman(start time.Time) string {
+	//TODO
+	//return time.Since(start).String()
+	diff := time.Since(start)
+	days := int(diff.Hours() / 24)
+	hours := int(diff.Hours()) - 24*days
+	minutes := int(diff.Minutes()) - 24*days*60 - hours
+	seconds := int(diff.Seconds()) - 60*minutes - (24*days*60-hours)*60
+	switch {
+	case days > 0:
+		return fmt.Sprintf("%dD %dH", days, hours)
+	case hours > 0:
+		return fmt.Sprintf("%dH %dM", hours, minutes)
+	default:
+		return fmt.Sprintf("%dM %dS", minutes, seconds)
+	}
+}
+
+/*
+//sources from gogs project
+func computeTimeDiff(diff int64) (int64, string) {
+	diffStr := ""
+	switch {
+	case diff <= 0:
+		diff = 0
+		diffStr = "now"
+	case diff < 2:
+		diff = 0
+		diffStr = "1 second"
+	case diff < 1*Minute:
+		diffStr = fmt.Sprintf("%d seconds", diff)
+		diff = 0
+
+	case diff < 2*Minute:
+		diff -= 1 * Minute
+		diffStr = "1 minute"
+	case diff < 1*Hour:
+		diffStr = fmt.Sprintf("%d minutes", diff/Minute)
+		diff -= diff / Minute * Minute
+
+	case diff < 2*Hour:
+		diff -= 1 * Hour
+		diffStr = "1 hour"
+	case diff < 1*Day:
+		diffStr = fmt.Sprintf("%d hours", diff/Hour)
+		diff -= diff / Hour * Hour
+
+	case diff < 2*Day:
+		diff -= 1 * Day
+		diffStr = "1 day"
+	case diff < 1*Week:
+		diffStr = fmt.Sprintf("%d days", diff/Day)
+		diff -= diff / Day * Day
+
+	case diff < 2*Week:
+		diff -= 1 * Week
+		diffStr = "1 week"
+	case diff < 1*Month:
+		diffStr = fmt.Sprintf("%d weeks", diff/Week)
+		diff -= diff / Week * Week
+
+	case diff < 2*Month:
+		diff -= 1 * Month
+		diffStr = "1 month"
+	case diff < 1*Year:
+		diffStr = fmt.Sprintf("%d months", diff/Month)
+		diff -= diff / Month * Month
+
+	case diff < 2*Year:
+		diff -= 1 * Year
+		diffStr = "1 year"
+	default:
+		diffStr = fmt.Sprintf("%d years", diff/Year)
+		diff = 0
+	}
+	return diff, diffStr
+}
+
+// TimeSincePro calculates the time interval and generate full user-friendly string. sources from gogs project
+func TimeSincePro(then time.Time) string {
+	now := time.Now()
+	diff := now.Unix() - then.Unix()
+
+	if then.After(now) {
+		return "future"
+	}
+
+	var timeStr, diffStr string
+	for {
+		if diff == 0 {
+			break
+		}
+
+		diff, diffStr = computeTimeDiff(diff)
+		timeStr += ", " + diffStr
+	}
+	return strings.TrimPrefix(timeStr, ", ")
+}
+//*/
--- routers/admin/admin.go
+++ routers/admin/admin.go
@@ -1,11 +1,102 @@
 package admin
 
 import (
+	"fmt"
+	"runtime"
+	"time"
+
 	"github.com/Unknwon/macaron"
 	"github.com/macaron-contrib/session"
 	"github.com/sapk/GoWatch/modules/auth"
 	"github.com/sapk/GoWatch/modules/db"
+	"github.com/sapk/GoWatch/modules/tools"
 )
+
+var (
+	startTime = time.Now()
+)
+
+type sysStatus struct {
+	Uptime       string
+	NumGoroutine int
+
+	// General statistics.
+	MemAllocated string // bytes allocated and still in use
+	MemTotal     string // bytes allocated (even if freed)
+	MemSys       string // bytes obtained from system (sum of XxxSys below)
+	Lookups      uint64 // number of pointer lookups
+	MemMallocs   uint64 // number of mallocs
+	MemFrees     uint64 // number of frees
+
+	// Main allocation heap statistics.
+	HeapAlloc    string // bytes allocated and still in use
+	HeapSys      string // bytes obtained from system
+	HeapIdle     string // bytes in idle spans
+	HeapInuse    string // bytes in non-idle span
+	HeapReleased string // bytes released to the OS
+	HeapObjects  uint64 // total number of allocated objects
+
+	// Low-level fixed-size structure allocator statistics.
+	//	Inuse is bytes used now.
+	//	Sys is bytes obtained from system.
+	StackInuse  string // bootstrap stacks
+	StackSys    string
+	MSpanInuse  string // mspan structures
+	MSpanSys    string
+	MCacheInuse string // mcache structures
+	MCacheSys   string
+	BuckHashSys string // profiling bucket hash table
+	GCSys       string // GC metadata
+	OtherSys    string // other system allocations
+
+	// Garbage collector statistics.
+	NextGC       string // next run in HeapAlloc time (bytes)
+	LastGC       string // last run in absolute time (ns)
+	PauseTotalNs string
+	PauseNs      string // circular buffer of recent GC pause times, most recent at [(NumGC+255)%256]
+	NumGC        uint32
+}
+
+func getSystemStatus() sysStatus {
+	var sys sysStatus
+	sys.Uptime = tools.TimeSinceHuman(startTime)
+
+	m := new(runtime.MemStats)
+	runtime.ReadMemStats(m)
+	sys.NumGoroutine = runtime.NumGoroutine()
+
+	sys.MemAllocated = tools.FileSize(int64(m.Alloc))
+	sys.MemTotal = tools.FileSize(int64(m.TotalAlloc))
+	sys.MemSys = tools.FileSize(int64(m.Sys))
+	sys.Lookups = m.Lookups
+	sys.MemMallocs = m.Mallocs
+	sys.MemFrees = m.Frees
+
+	sys.HeapAlloc = tools.FileSize(int64(m.HeapAlloc))
+	sys.HeapSys = tools.FileSize(int64(m.HeapSys))
+	sys.HeapIdle = tools.FileSize(int64(m.HeapIdle))
+	sys.HeapInuse = tools.FileSize(int64(m.HeapInuse))
+	sys.HeapReleased = tools.FileSize(int64(m.HeapReleased))
+	sys.HeapObjects = m.HeapObjects
+
+	sys.StackInuse = tools.FileSize(int64(m.StackInuse))
+	sys.StackSys = tools.FileSize(int64(m.StackSys))
+	sys.MSpanInuse = tools.FileSize(int64(m.MSpanInuse))
+	sys.MSpanSys = tools.FileSize(int64(m.MSpanSys))
+	sys.MCacheInuse = tools.FileSize(int64(m.MCacheInuse))
+	sys.MCacheSys = tools.FileSize(int64(m.MCacheSys))
+	sys.BuckHashSys = tools.FileSize(int64(m.BuckHashSys))
+	sys.GCSys = tools.FileSize(int64(m.GCSys))
+	sys.OtherSys = tools.FileSize(int64(m.OtherSys))
+
+	sys.NextGC = tools.FileSize(int64(m.NextGC))
+	sys.LastGC = fmt.Sprintf("%.1fs", float64(time.Now().UnixNano()-int64(m.LastGC))/1000/1000/1000)
+	sys.PauseTotalNs = fmt.Sprintf("%.1fs", float64(m.PauseTotalNs)/1000/1000/1000)
+	sys.PauseNs = fmt.Sprintf("%.3fs", float64(m.PauseNs[(m.NumGC+255)%256])/1000/1000/1000)
+	sys.NumGC = m.NumGC
+
+	return sys
+}
 
 // Dashboard genrate the home admin page
 func Dashboard(ctx *macaron.Context, auth *auth.Auth, sess session.Store, db *db.Db) {
@@ -13,6 +104,8 @@ func Dashboard(ctx *macaron.Context, auth *auth.Auth, sess session.Store, db *db
 		return
 	}
 	fillGlobalPage(ctx, db, "admin_dashboard")
+
+	ctx.Data["SysStatus"] = getSystemStatus()
 	ctx.HTML(200, "admin/dashboard")
 }
 
